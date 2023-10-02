50bb31f284de52cb806c0afc134db298cb9d1c17
Adna Farias <92997026+adnalisia@users.noreply.github.com>
2023-10-02 15:32:25 -0300

    adicionando o timer

---
+++ timer.py
@@ -0,0 +1,19 @@
+# Importando os pacotes necessários
+import pygame as pg
+import random
+
+class TimerGame:
+    def _init_(self):
+        self.tempo_comeco = pg.time.get_ticks()
+        self.tempo_limite_segundos = 90
+        self.meta = random.randint(0, 100)
+        self.valor_inicial = 1
+
+    def atualizar(self):
+        tempo_atual = pg.time.get_ticks()
+        tempo_decorrido_segundos = (tempo_atual - self.tempo_comeco) // 1000
+        tempo_restante = max(self.tempo_limite_segundos - tempo_decorrido_segundos, 0)
+        return tempo_restante
+jogo = TimerGame()
+#dentro do loop:
+tempo_restante = jogo.atualizar()
